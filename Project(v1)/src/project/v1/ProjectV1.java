908817c197514fa4eb1680168662da577e453ca5
Mindaugas <mindaugasdirg@gmail.com>
2017-11-22 13:07:43 +0200

    Add project files

---
+++ Project(v1)/src/project/v1/ProjectV1.java
@@ -0,0 +1,20 @@
+/* 
+ * Project for data structure course in university
+ * Mindaugas DirginÄ�ius, 2017
+ */
+package project.v1;
+
+/**
+ *
+ * @author Mindaugas
+ */
+public class ProjectV1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
