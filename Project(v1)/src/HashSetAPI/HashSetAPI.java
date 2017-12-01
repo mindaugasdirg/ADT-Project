5c76f23b9eec4e25d4daa08deaed16924af993fd
Mindaugas <mindaugasdirg@gmail.com>
2017-12-01 17:52:47 +0200

    Add clear method to HashSetAPI interface

--- Project(v1)/src/HashSetAPI/HashSetAPI.java
+++ Project(v1)/src/HashSetAPI/HashSetAPI.java
@@ -12,6 +12,7 @@ package HashSetAPI;
 public interface HashSetAPI<K, E> extends Iterable<E>{
     void add(K k, E e);
     void remove(K k);
+    void clear();
     E get(K k);
     Object[] getKeys();
     int size();
